c5e137e07d0e31db9c9a6ca20a82c76cde3e84ab
pashaba <kimtha293@gmail.com>
2025-02-01 18:10:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,4 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
 event.preventDefault();  // Mencegah form dari refresh otomatis
 
-    // Jika form di-submit, langsung arahkan ke halaman lain
-    window.location.href = 'https://center.vpskita.my.id';  // Ganti dengan URL tujuan
-});
-```
+   
